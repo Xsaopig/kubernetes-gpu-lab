d4c0edee87ad49f17faac9ee2c05acb28a6256d6
Saopig X <996131974@qq.com>
2024-12-25 03:25:53 +0800

    frontend-go main.go

--- src/frontend-go/imagebuild.sh
+++ src/frontend-go/imagebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 go build -o frontend-go
-docker build -t frontend-go:latest ./
-docker stop frontend-go
-docker rm frontend-go
-docker create --name=frontend-go -p 6999:6999 frontend-go:latest
-docker start frontend-go
+# docker build -t frontend-go:latest ./
+# docker stop frontend-go
+# docker rm frontend-go
+# docker create --name=frontend-go -p 6999:6999 frontend-go:latest
+# docker start frontend-go
